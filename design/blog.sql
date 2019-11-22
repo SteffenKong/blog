755a577de8ddf1052d023426109070012aa803e7
SteffenKong <3266023724@qq.com>
2019-11-22 17:31:54 +0800

    fix:搭建框架

---
+++ design/blog.sql
@@ -0,0 +1,113 @@
+create database if not exists blog charset=utf8;
+use blog;
+
+-- 后台管理用户
+create table if not exists blog_admin(
+    id mediumint unsigned auto_increment,
+    account varchar(191) not null comment '账号名',
+    password varchar(191) not null comment '密码',
+    email varchar(191) not null comment '邮箱',
+    image varchar(191) comment '头像',
+    last_login_ip comment '最后一次登录的ip',
+    last_login_time datetime comment '最后一次登录的时间',
+    created_at int not null comment '添加时间',
+    updated_at int not null comment '编辑时间',
+    primary key (id),
+    unique key uk_account(account),
+    unique key uk_email(email)
+)charset=utf8,engine=innodb;
+
+-- 文章标签表
+create table if not exists blog_tags(
+    id mediumint unsigned auto_increment,
+    title varchar(64) not null comment '标签名',
+    status tinyint default 1 comment '状态 0 - 禁用 1 - 启用',
+    description text not null comment '描述',
+    created_at int not null comment '添加时间',
+    updated_at int not null comment '编辑时间',
+    primary key (id),
+    unique key uk_title(title),
+    key uk_status(status)
+)charset=utf8,engine=innodb;
+
+-- 文章分类表
+create table if not exists blog_category(
+    id mediumint unsigned auto_increment,
+    title varchar(64) not null comment '分类名',
+    pid mediumint unsigned not null comment '上级id',
+    status tinyint default 1 comment '状态 0 - 禁用 1 - 启用',
+    description text not null comment '描述',
+    created_at int not null comment '添加时间',
+    updated_at int not null comment '编辑时间',
+    primary key(id),
+    unique key uk_title(title),
+    key idx_status(status),
+    key idx_pid(pid)
+)charset=utf8,engine=innodb;
+
+-- 友情连接
+create table if not exists blog_link(
+    id mediumint unsigned not null auto_increment,
+    title varchar(191) not null comment '友情连接名',
+    url varchar(191) not null comment '友情链接url',
+    status tinyint default 1 comment '状态 0 - 禁用 1 - 启用',
+    sort int default 50 comment '排序值,越小越靠前',
+    created_at int not null comment '添加时间',
+    updated_at int not null comment '编辑时间',
+    primary key (id),
+    unique key uk_title (title)
+    unique key uk_url (url)
+)charset=utf8,engine=innodb;
+
+-- 文章表
+create table if not exists blog_article(
+    id mediumint unsigned not null auto_increment,
+    title varchar(191) not null comment '标题',
+    description varchar(255) not null comment '文章简介'
+    big_image varchar(255) not null comment '原图路径',
+    small_image varchar(255) not null comment '缩略图路径',
+    status tinyint default 1 comment '状态 0 - 禁用 1 - 启用',
+    is_hot tinyint default 0 comment '是否热销',
+    is_rec tinyint default 0 comment '是否推荐',
+    view_number int default 0 comment '浏览数',
+    author varchar(191) not null comment '作者昵称',
+    created_at int not null comment '添加时间',
+    updated_at int not null comment '编辑时间',
+    primary key (id),
+    unique key uk_title (title),
+    key idx_status (status),
+    key idx_is_hot (is_hot),
+    key idx_is_rec (is_rec),
+    key idx_view_number (view_number)
+)charset=utf8,engine=innodb;
+
+
+-- 文章内容表
+create table if not exists blog_article_details(
+    id mediumint unsigned not null auto_increment,
+    article_id mediumint unsigned not null comment '文章id',
+    content text not null comment '文章内容',
+    created_at int not null comment '添加时间',
+    updated_at int not null comment '编辑时间',
+    primary key (id),
+    key idx_article_id (article_id)
+)charset=utf8,engine=innodb;
+
+
+-- 评论表
+create table if not exists blog_comment(
+    id mediumint unsigned not null auto_increment,
+    username varchar(191)  comment '评论者姓名',
+    email varchar(191) comment '评论者邮箱',
+    pid tinyint default 0 comment '回复者id,0为评论者',
+    content text comment '评论内容',
+    is_verify tinyint default 0 comment '0 - 待审核 1 - 审核不通过 2 - 审核通过',
+    verify_time datetime not null comment '审核时间',
+    created_at int not null comment '添加时间',
+    updated_at int not null comment '编辑时间',
+    primary key (id),
+    key idx_username (username),
+    key idx_email (email),
+    key idx_pid (pid),
+    key idx_is_verify (is_verify)
+)charset=utf8,engine=innodb;
